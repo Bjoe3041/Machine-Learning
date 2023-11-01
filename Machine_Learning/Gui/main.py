0690ada98fd6b64f461f93ef291d3d77306ec2ff
MagnusPC <103988876+MagnusPC@users.noreply.github.com>
2023-11-01 11:25:16 +0100

    added second mode to main

--- Machine_Learning/Gui/main.py
+++ Machine_Learning/Gui/main.py
@@ -1,11 +1,15 @@
 from Unparted_file import UnpartedFile
+from Parted_file import PartedFile
 from MenuHoster import MenuHoster
 
 mode = "notconsole"
 
-if mode == "console":
+if mode == "console_UP":
     uf = UnpartedFile()
     uf.run_all()
+elif mode == "console_P":
+    pf = PartedFile()
+    pf.run_all()
 else:
     menu = MenuHoster()
     menu.hostmenu()
